138c11f1584e3e54ef98bf6caa5f9d4106321eb3
afif hendrawan <afif.hendrawan@gmail.com>
2017-03-17 22:58:13 +0700

    gui done

--- src/timer/MainWindow.java
+++ src/timer/MainWindow.java
@@ -17,7 +17,7 @@ import javax.swing.JTextField;
  */
 public class MainWindow {
     JFrame mainFrame;
-    JButton startBtn, stopBtn, getOutputBtn;
+    JButton startBtn, stopBtn, outputBtn;
     JPanel timerPanel, startStopPanel, outputPanel;
     JLabel timerText;
     
@@ -35,66 +35,66 @@ public class MainWindow {
         mainFrame.setLayout(mainLayout);
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
         
         //Timer Text Panel
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.5;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
         timerPanel = new JPanel();
         timerPanel.setBorder(BorderFactory.createTitledBorder(Constants.TIMER_PANEL_TITLE));
         timerPanel.setLayout(new GridBagLayout());
         GridBagConstraints timerPanelCons = new GridBagConstraints();
         timerPanelCons.fill = GridBagConstraints.HORIZONTAL;
         
-        timerPanelCons.gridx = 0;
-        timerPanelCons.gridy = 0;
+        //timerPanelCons.gridx = 0;
+        //timerPanelCons.gridy = 0;
         timerText = new JLabel("0.0");
         timerText.setFont(new Font(Constants.LUCIA_GRANDE, Font.BOLD, 50));
         timerPanel.add(timerText, timerPanelCons);
         
-        //Start Stop Panel
+        mainFrame.add(timerPanel, gbc);
+        
+        // ============================================== //
+        // ============ Button Panel ==================== //
         gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.5;
-        startStopPanel = new JPanel();
-        startStopPanel.setBorder(BorderFactory.createTitledBorder(Constants.START));
-        startStopPanel.setLayout(new GridBagLayout());
-        GridBagConstraints startStopPanelCons = new GridBagConstraints();
-        startStopPanelCons.fill = GridBagConstraints.HORIZONTAL;
-        
-        //Start Button
-        startStopPanelCons.gridx = 0;
-        startStopPanelCons.gridy = 0;
-        startStopPanelCons.weightx = 0.5;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
         startBtn = new JButton();
         startBtn.setFont(new Font(Constants.LUCIA_GRANDE, Font.PLAIN, 14));
         startBtn.setText(Constants.START);
         startBtn.addActionListener((ActionEvent e) -> {
             System.out.println("From Start Button");
         });
-        startStopPanel.add(startBtn, startStopPanelCons);
+        mainFrame.add(startBtn, gbc);
         
-        //Stop Button
-        startStopPanelCons.gridx = 1;
-        startStopPanelCons.gridy = 0;
-        startStopPanelCons.weightx = 0.5;
+        // Stop Button
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
         stopBtn = new JButton();
         stopBtn.setFont(new Font(Constants.LUCIA_GRANDE, Font.PLAIN, 14));
         stopBtn.setText(Constants.STOP);
         stopBtn.addActionListener((ActionEvent e) -> {
             System.out.println("From Stop Button");
         });
-        startStopPanel.add(stopBtn, startStopPanelCons);
+        mainFrame.add(stopBtn, gbc);
         
-        
-        
-        
-        
-        //Add All Panel to Main Frame
-        mainFrame.add(timerPanel, gbc);
-        mainFrame.add(startStopPanel, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+        outputBtn = new JButton();
+        outputBtn.setFont(new Font(Constants.LUCIA_GRANDE, Font.PLAIN, 14));
+        outputBtn.setText(Constants.OUTPUT);
+        outputBtn.addActionListener((ActionEvent e) -> {
+            System.out.println("From Output Button");
+        });
+        mainFrame.add(outputBtn, gbc);
         
         //Set MainFrame to Visibel
         mainFrame.setVisible(true);
